c397deb45265e1f9f5759f8e34632d43d9d48864
Hassan Ali <helfi92@gmail.com>
2018-11-13 21:53:57 -0500

    Lint scripts directory

--- scripts/npm-adduser.js
+++ scripts/npm-adduser.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 // eslint-disable-next-line import/no-unresolved
 const npm = require('npm');
+
 const REGISTRY = 'http://localhost:4873';
 const email = 'test@test.org';
 const username = 'test';
@@ -12,20 +13,23 @@ npm.load({}, err => {
   }
 
   const auth = { username, password, email };
+
   npm.config.set('registry', REGISTRY, 'user');
   npm.config.set('email', email, 'user');
   npm.registry.adduser(REGISTRY, { auth }, (err, doc) => {
     if (err) {
       throw err;
     }
+
     if (!doc || !doc.token) {
       throw new Error('No auth token');
     }
+
     npm.config.setCredentialsByURI(REGISTRY, { token: doc.token });
     npm.config.save('user', err => {
       if (err) {
         throw err;
       }
-    })
+    });
   });
 });
