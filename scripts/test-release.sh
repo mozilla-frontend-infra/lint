a55e62f5f85cc22d195a090f101b4845aeec0325
Hassan Ali <helfi92@gmail.com>
2018-11-13 21:47:45 -0500

    Nit

--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -7,6 +7,9 @@ npm config set registry http://localhost:4873/;
 # Add npm user so we can use it to publish
 scripts/npm-adduser.js;
 
+# Delete its corresponding verdaccio storage so that we don't have to change the version in order to publish
+rm -rf $HOME/.local/share/verdaccio/storage/@mozilla-frontend-infra/lint
+
 lerna publish --yes --no-verify-access --no-git-tag-version --no-push;
 
 
