a4f9b864d8f225f16fb36bc359ab62991dac0f96
Hassan Ali <helfi92@gmail.com>
2019-10-20 13:08:31 -0400

    Breaking: Migrate neutrino packages to 9.0.0-rc.4 (#14)
    ```js
    // Before without options:
    module.exports = {
      use: [
        '@mozilla-frontend-infra/react-lint'
      ]
    };
    
    // After without options:
    const reactLint = require('@mozilla-frontend-infra/react-lint');
    
    module.exports = {
      use: [
        reactLint()
      ]
    };
    
    // Before with options:
    module.exports = {
      use: [
        ['@mozilla-frontend-infra/react-lint', options]
      ]
    };
    
    // After with options:
    const reactLint = require('@mozilla-frontend-infra/react-lint');
    
    module.exports = {
      use: [
        reactLint(options)
      ]
    };
    ```

--- packages/node-lint/index.js
+++ packages/node-lint/index.js
@@ -2,10 +2,12 @@ const airbnb = require('@neutrinojs/airbnb-base');
 const lint = require('@mozilla-frontend-infra/lint');
 const merge = require('deepmerge');
 
-module.exports = (neutrino, options = {}) => {
-  neutrino.use(lint, {
-    use: [airbnb],
-  });
+module.exports = (options = {}) => neutrino => {
+  neutrino.use(
+    lint({
+      use: [airbnb],
+    })
+  );
 
   neutrino.config.module
     .rule('lint')
